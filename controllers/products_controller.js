a1d03287e53b18ed5fe36cc75717893d0aff55c3
djkoser <koser.david@gmail.com>
2021-03-20 14:52:26 -0500

    Finish lab

---
+++ controllers/products_controller.js
@@ -0,0 +1,52 @@
+module.exports = {
+  create: ( req, res, next ) => {
+    const dbInstance = req.app.get('db');
+    const { name, description, price, image_url } = req.body;
+    dbInstance.create_product([ name, description, price, image_url ])
+      .then( () => res.sendStatus(200) )
+      .catch( err => {
+        res.status(500).send("Error, we are working as quickly as possible to solve this issue.");
+        console.log(err)
+      } );
+    },
+  getOne: (req,res,next)=> {
+    const dbInstance = req.app.get('db');
+
+    dbInstance.read_product([req.params.id])
+    .then((product) => res.status(200).send(product))
+    .catch(err => {
+      res.status(500).send("Error, we are working as quickly as possible to solve this issue.");
+      console.log(err);
+    });
+  },
+  getAll: (req,res,next)=> {
+    const dbInstance = req.app.get('db');
+    dbInstance.read_products()
+    .then((product) => res.status(200).send(product))
+    .catch(err => {
+      res.status(500).send("Error, we are working as quickly as possible to solve this issue.");
+      console.log(err);
+    });
+
+  },
+  update: (req,res,next) => {
+    const dbInstance = req.app.get('db');
+
+    dbInstance.update_product([req.params.id, req.query.desc])
+    .then((product) => res.status(200).send(product))
+    .catch(err => {
+      res.status(500).send("Error, we are working as quickly as possible to solve this issue.");
+      console.log(err);
+    });
+  },
+  delete: (req,res,next) => {
+    const dbInstance = req.app.get('db');
+
+    dbInstance.delete_product(req.params.id)
+    .then((product) => res.status(200).send(product))
+    .catch(err => {
+      res.status(500).send("Error, we are working as quickly as possible to solve this issue.");
+      console.log(err);
+    });
+  }
+}
---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const massive = require('massive');
+const products_controller = require('./controllers/products_controller');
+const app = express(); 
+
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+
+app.use(express.json());
+
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: {rejectUnauthorized: false}
+  })
+.then(dbInstance => {
+    app.set('db', dbInstance);
+    app.listen(SERVER_PORT, () => console.log(`Database mounted and server listening on port ${SERVER_PORT}.`));
+  })
+.catch((err)=> console.log(err));
+
+app.get('/api/products', products_controller.getAll);
+app.get('/api/products/:id', products_controller.getOne);
+app.put('/api/products/:id', products_controller.update);
+app.post('/api/products', products_controller.create);
+app.delete('/api/products/:id', products_controller.delete);
+
